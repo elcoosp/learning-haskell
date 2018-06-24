7d56607f2605f26c26fb6345957f50382e29bfb5
Elcoosp <elcoosp@gmail.com>
2018-06-24 23:51:48 +0200

    Add zip implementation

--- wikibook/Elementary/recursion.hs
+++ wikibook/Elementary/recursion.hs
@@ -1,8 +1,8 @@
 -- Product of every other number from 1 (or 2) up to n
-doublefactorial :: Int -> Int
-doublefactorial n
+doubleFactorial :: Int -> Int
+doubleFactorial n
   | n <= 0 = 1
-  | n >= 1 = doublefactorial (n - 2) * n
+  | n >= 1 = doubleFactorial (n - 2) * n
 
 -- Put a int x to the y power
 power :: (Num a, Eq a) => a -> a -> a
@@ -42,3 +42,13 @@ atIdx (x:xs) i
   | i > length xs || i < 0 = error "Index out of range"
   | i == 0 = x
   | otherwise = atIdx xs (i - 1)
+
+-- Return a tuple of the heads from the two lists
+heads :: [a] -> [b] -> (a, b)
+heads x y = (head x, head y)
+
+-- Takes two lists and 'zips' them together, so that the first pair in the resulting list is the first two elements of the two lists, and so on
+zip' :: [a] -> [b] -> [(a, b)]
+zip' xs xxs
+  | length xs == 1 || length xxs == 1 = [heads xs xxs]
+  | otherwise = heads xs xxs : zip' (tail xs) (tail xxs)
