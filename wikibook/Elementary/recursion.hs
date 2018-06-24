a1ed7934b11a2c5ceb705717f4e10438bac8a9e6
Elcoosp <elcoosp@gmail.com>
2018-06-24 21:42:36 +0200

    Add atIdx function

--- wikibook/Elementary/recursion.hs
+++ wikibook/Elementary/recursion.hs
@@ -1,8 +1,8 @@
 -- Product of every other number from 1 (or 2) up to n
 doublefactorial :: Int -> Int
 doublefactorial n
-    | n <= 0 = 1
-    | n >= 1 = doublefactorial (n - 2) * n
+  | n <= 0 = 1
+  | n >= 1 = doublefactorial (n - 2) * n
 
 -- Put a int x to the y power
 power :: (Num a, Eq a) => a -> a -> a
@@ -17,9 +17,9 @@ plusOne x = x + 1
 -- Add two ints together
 addition :: Int -> Int -> Int
 addition x y
-    | y == 0 = x
-    | y < 0 = addition (x - 1) (plusOne y)
-    | otherwise =  addition (plusOne x) (y - 1)
+  | y == 0 = x
+  | y < 0 = addition (x - 1) (plusOne y)
+  | otherwise = addition (plusOne x) (y - 1)
 
 -- Log base 2
 log2 :: Double -> Double
@@ -28,9 +28,17 @@ log2 = logBase 2
 -- ########################
 -- #### List recursion ####
 -- ########################
-
+--
 -- Takes a count and an element and returns the list which is that element repeated that many times.
 replicate' :: Int -> a -> [a]
-replicate' i x = case (i, x) of 
-    (0, _)  -> []
-    _  -> x : replicate' (i - 1) x
+replicate' i x =
+  case (i, x) of
+    (0, _) -> []
+    _ -> x : replicate' (i - 1) x
+
+-- Element at the given index
+atIdx :: [a] -> Int -> a
+atIdx (x:xs) i
+  | i > length xs || i < 0 = error "Index out of range"
+  | i == 0 = x
+  | otherwise = atIdx xs (i - 1)
